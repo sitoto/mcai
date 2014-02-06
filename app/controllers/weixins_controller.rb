a5398e8717d005588de692496fed1ac84ee4a384
sitoto <huang.gadela@gmail.com>
2014-02-06 17:35:06 +0800

    add  lamp return

--- app/controllers/weixins_controller.rb
+++ app/controllers/weixins_controller.rb
@@ -4,35 +4,35 @@ class WeixinsController < ApplicationController
   skip_before_filter :verify_authenticity_token
   before_filter :check_weixin_legality
 
-  
+
   # GET /weixins
-#  def index
-#    @weixins = Weixin.all
-#  end
+  #  def index
+  #    @weixins = Weixin.all
+  #  end
 
   def show
     render :text => params[:echostr]
   end
 
-#  POST /weixin
-#  def create
-#   @weixin = Weixin.new(weixin_params)
+  #  POST /weixin
+  #  def create
+  #   @weixin = Weixin.new(weixin_params)
 
-#   if @weixin.save
-#    redirect_to @weixin, notice: 'Weixin was successfully created.'
-#   else
-#     render action: 'new'
-#    end
-#  end
+  #   if @weixin.save
+  #    redirect_to @weixin, notice: 'Weixin was successfully created.'
+  #   else
+  #     render action: 'new'
+  #    end
+  #  end
   def create
     query_type = params[:xml][:MsgType]
     query_content = params[:xml][:Content]
-   
+
     if query_type == "text"
       if  query_content == "Hello2BusUser"
         render "welcome", :formats => :xml
       else
-        @echostr = "对不起，暂未查询到数据，我正在完善数据库，欢迎过会再来查。"
+        @echostr = get_lamp_info(car_info)
         render "echo", :formats => :xml
       end
     elsif query_type == "image"
@@ -40,26 +40,30 @@ class WeixinsController < ApplicationController
     end
   end
 
-
-
-  # PATCH/PUT /weixins/1
-  def update
-    if @weixin.update(weixin_params)
-      redirect_to @weixin, notice: 'Weixin was successfully updated.'
-    else
-      render action: 'edit'
-    end
-  end
-
   private
   # Use callbacks to share common setup or constraints between actions.
   def set_weixin
-#    @weixin = Weixin.find(params[:id])
+    #    @weixin = Weixin.find(params[:id])
+  end
+
+  def get_lamp_info(car_info)
+    #变大写
+    #去掉空格，成为数组| 成为一条语句
+
+    lamp = Lamp.any_of({maker: Regexp.new(".*"+car_info+".*")}, {model: Regexp.new(".*"+car_info+".*")} ).first
+
+    if lamp
+      "#{t('lamps.high_beam')}:#{lamp.high_beam}"
+    else
+      "对不起，暂未查询到数据，我正在完善数据库，欢迎过会再来查。"
+    end
+
+
   end
 
   # Only allow a trusted parameter "white list" through.
   def weixin_params
-#    params.require(:weixin).permit(:name, :ask, :answer, :words_count)
+    #    params.require(:weixin).permit(:name, :ask, :answer, :words_count)
   end
 
   # 根据参数校验请求是否合法，如果非法返回错误页面
